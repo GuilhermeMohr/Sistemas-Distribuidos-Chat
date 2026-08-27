6600ee32938ccb9a02fe7dc9028f48e2c592b40b
Guilherme Mohr <72572592+GuilhermeMohr@users.noreply.github.com>
2026-08-26 21:14:10 -0300

    Correção de erros e input

--- multicast.py
+++ multicast.py
@@ -2,69 +2,78 @@ import socket
 import threading
 import json
 import sys
+import struct
+import time
 
 # Const
-NUMBER_OF_PROCESSES = 3
+MULTICAST_GROUP = "239.0.0.1"
 MULTICAST_PORT = 50000
 
-# Variables
-host = socket.gethostbyname(socket.gethostname())
-
 def create_multicast_socket():
-    # multicast_socket = socket.socket(
-    #     socket.AF_INET,
-    #     socket.SOCK_DGRAM
-    #     # socket.IPPROTO_UDP
-    # )
-
     multicast_socket = socket.socket(
         socket.AF_INET,
-        socket.SOCK_DGRAM
+        socket.SOCK_DGRAM,
+        socket.IPPROTO_UDP
     )
 
-    # Allow several processes to bind to the same UDP port.
     multicast_socket.setsockopt(
         socket.SOL_SOCKET,
         socket.SO_REUSEADDR,
         1
     )
 
-    multicast_socket.bind((host, MULTICAST_PORT))
+    multicast_socket.bind(
+        ("", MULTICAST_PORT)
+    )
+
+    membership = struct.pack(
+        "4s4s",
+        socket.inet_aton(MULTICAST_GROUP),
+        socket.inet_aton("0.0.0.0")
+    )
+
+    multicast_socket.setsockopt(
+        socket.IPPROTO_IP,
+        socket.IP_ADD_MEMBERSHIP,
+        membership
+    )
 
     return multicast_socket
 
 def receive_multicast(multicast_socket, process_id):
     while True:
         try:
-            threading.Thread(target=handle_message, args=(multicast_socket, process_id)).start()
+            data, address = multicast_socket.recvfrom(1024)
+            message = json.loads(data.decode("utf-8"))
 
+            if (message['id'] == process_id):
+                continue
+
+            print(f"{process_id}: Mensagem recebida: {message['message']}\n")
         except Exception as error:
             print(f"{process_id}: error: {error}\n")
 
-def handle_message(multicast_socket, process_id):
-    try:
-        data, address = multicast_socket.recvfrom(1024)
-        message = json.loads(data.decode("utf-8"))
+def write(multicast_socket, process_id):
+    while True:
+        print("Enviar mensagem: ")
+        message = {
+            "id": process_id,
+            "message": input()
+        }
 
-        print(f"{process_id}: Mensagem recebida: {message['message']}\n")
-    except:
-        print(f"Erro!\n")
+        try:
+            broadcast(multicast_socket, message)
+        except:
+            print(f"{process_id}: ERRO!\n")
 
-def write(multicast_socket, host, process_id):
-    message = {
-        "message": "Olá",
-    }
-
-    try:
-        broadcast(multicast_socket, host, message)
-    except:
-        print(f"{process_id}: ERRO!\n")
-
-def broadcast(multicast_socket, host, message):
-    multicast_socket.sendto(json.dumps(message).encode("utf-8"), (host, MULTICAST_PORT))
+def broadcast(multicast_socket, message):
+    multicast_socket.sendto(json.dumps(message).encode("utf-8"), (MULTICAST_GROUP, MULTICAST_PORT))
 
 process_id = int(sys.argv[1])
 multicast_socket = create_multicast_socket()
 
 threading.Thread(target=receive_multicast, args=(multicast_socket, process_id)).start()
-threading.Thread(target=write, args=(multicast_socket, host, process_id)).start()
+
+time.sleep(2)
+
+threading.Thread(target=write, args=(multicast_socket, process_id)).start()
--- run.ps1
+++ run.ps1
@@ -1,3 +1,3 @@
-1..10 | ForEach-Object {
+1..3 | ForEach-Object {
     Start-Process python -ArgumentList "./multicast.py $_"
 }
